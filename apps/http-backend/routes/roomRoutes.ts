7eba9d1987c8269b774f3da8ccec68f7ed92cedb
YadlaMani <yadlamaniymn2005@gmail.com>
2025-03-31 22:40:06 +0530

    http-backend fix2

--- apps/http-backend/routes/roomRoutes.ts
+++ apps/http-backend/routes/roomRoutes.ts
@@ -9,10 +9,11 @@ import {
 } from "../controllers/roomController";
 
 const router = express.Router();
-export default router;
+
 router.post("/create", createRoom);
 router.get("/chats/:roomId", getRoomChats);
 router.post("/roomId", getRoomId);
 router.get("/", getRooms);
 router.get("/shapes/:roomId", getRoomShapes);
 router.get("/:roomSlug", getRoomDetails);
+export default router;
