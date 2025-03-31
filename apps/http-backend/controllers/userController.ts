94c4b68048c093432a877d60f22f332ef069ec32
YadlaMani <yadlamaniymn2005@gmail.com>
2025-03-31 22:48:22 +0530

    http-backend fix4

--- apps/http-backend/controllers/userController.ts
+++ apps/http-backend/controllers/userController.ts
@@ -1,9 +1,24 @@
 import type { Request, Response } from "express";
-import { userSchema, loginSchema } from "@repo/common/utils";
+
 import { prismaClient } from "@repo/db/client";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-
+import { z } from "zod";
+const userSchema = z.object({
+  username: z
+    .string()
+    .min(3, "Username must be at least 3 characters long")
+    .max(20, "Username must be at most 20 characters long"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+  email: z.string().email("Invalid email address"),
+});
+const loginSchema = z.object({
+  username: z
+    .string()
+    .min(3, "Username must be at least 3 characters long")
+    .max(20, "Username must be at most 20 characters long"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
 export const registerUser = async (req: Request, res: Response) => {
   try {
     const { username, password, email } = userSchema.parse(req.body);
